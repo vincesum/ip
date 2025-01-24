2d18aaf41f563d10daa42487f3cc67498fa0979c
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-01-24 21:49:04 +0800

    Renamed chatbot to Vinbot
    Added greeting and bye message

---
+++ src/main/java/Vinbot.java
@@ -0,0 +1,9 @@
+public class Vinbot {
+    public static void main(String[] args) {
+        String name = "Vinbot";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
