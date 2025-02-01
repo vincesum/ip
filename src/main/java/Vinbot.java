a4d5866ba79839cabcaf78a20b41f49bb5aca13e
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-02-01 15:45:27 +0800

    Add echo functionality
    Added function to switch off bot when user inputs "bye". Added personality to the bot by adding emptyline.

--- src/main/java/Vinbot.java
+++ src/main/java/Vinbot.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Vinbot {
     public static void main(String[] args) {
-        String name = "Vinbot";
+        boolean isActive = true;    //Defines the operation status of the bot
+        //Initialisation procedure
+        String name = "Vinbot ^-^";
+        String emptyLine = "----<<<<####****####>>>>----";
         System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?\n");
+        System.out.println(emptyLine);
+        System.out.print("What can I do for you?\n" + emptyLine + "\n");
+        //Echo
+        Scanner in = new Scanner(System.in);
+        while (isActive) {
+            String line;
+            line = in.nextLine();
+            if (line.toLowerCase().equals("bye")) {
+                isActive = false;
+                break;
+            }
+            System.out.println(emptyLine);
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
 
     }
