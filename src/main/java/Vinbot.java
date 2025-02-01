3b0737551feb1656a85412ae5f971fe146e1f5f4
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-02-01 16:01:34 +0800

    Add addtask functionality to store task in array
    Add list function to display current tasks
    Updated the indentation and lines of the bot to fit personality

--- src/main/java/Vinbot.java
+++ src/main/java/Vinbot.java
@@ -1,3 +1,5 @@
+import com.sun.security.auth.UnixNumericGroupPrincipal;
+
 import java.util.Scanner;
 
 public class Vinbot {
@@ -6,20 +8,36 @@ public class Vinbot {
         //Initialisation procedure
         String name = "Vinbot ^-^";
         String emptyLine = "----<<<<####****####>>>>----";
-        System.out.println("Hello! I'm " + name);
-        System.out.println(emptyLine);
-        System.out.print("What can I do for you?\n" + emptyLine + "\n");
+        String starLine = "****************************";
+        System.out.println("    Hello! I'm " + name);
+        System.out.println("    " + emptyLine);
+        System.out.print("    What can I do for you?\n" + "    " + emptyLine + "\n");
+        //Storage
+        String[] storage = new String[100];
+        int numberOfElements = 0;
         //Echo
         Scanner in = new Scanner(System.in);
         while (isActive) {
             String line;
             line = in.nextLine();
-            if (line.toLowerCase().equals("bye")) {
+            if (line.toLowerCase().equals("bye")) {    //exit loop
                 isActive = false;
                 break;
             }
-            System.out.println(emptyLine);
-            System.out.println(line);
+            if (line.toLowerCase().equals("list")) {    //displays list in array
+                int i = 0;
+                while (storage[i] != null) {
+                    System.out.println(i + 1 + ") " + storage[i]);
+                    i++;
+                }
+            }
+            else {
+                storage[numberOfElements] = line;
+                numberOfElements++;
+                System.out.println("    " + emptyLine);
+                System.out.println("    " + "added: "+ line);
+                System.out.println("    " + starLine);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
 
