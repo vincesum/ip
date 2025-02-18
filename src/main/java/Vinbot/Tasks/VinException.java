ca6519c97d99b5f3d5a3a2e21e9c158cc8141b1c
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-02-18 19:24:44 +0800

    Update exception

--- src/main/java/Vinbot/Tasks/VinException.java
+++ src/main/java/Vinbot/Tasks/VinException.java
@@ -1,11 +1,7 @@
 package Vinbot.Tasks;
 
 public class VinException extends Exception {
-    private String message;
     public VinException(String message) {
-        this.message = message;
-    }
-    public String getMessage() {
-        return message;
+        super(message);
     }
 }
