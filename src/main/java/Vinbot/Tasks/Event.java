43ca5da99d33a50b8fc964fea004d4b055a89d2c
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-03-04 23:47:55 +0800

    Add date class
    Fix bug where events and deadlines does not save in storage due to incorrect formatting of storage.

--- src/main/java/Vinbot/Tasks/Event.java
+++ src/main/java/Vinbot/Tasks/Event.java
@@ -1,5 +1,6 @@
 package Vinbot.Tasks;
 
+import Vinbot.Date;
 import Vinbot.UI;
 import Vinbot.VinException;
 
@@ -28,7 +29,10 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        //Return date data in specified format
+        Date newStart = new Date(start);
+        Date newEnd = new Date(end);
+        return "[E]" + super.toString() + " (from: " + newStart.getDate() + " to: " + newEnd.getDate() + ")";
     }
 
     @Override
@@ -37,7 +41,9 @@ public class Event extends Task {
     }
 
     public String getDate() {
-        return "(from: " + start + " to: " + end +  ")";
+        Date newStart = new Date(start);
+        Date newEnd = new Date(end);
+        return "(from: " + newStart.getDate() + " to: " + newEnd.getDate() +  ")";
     }
 
     public static String[] scan(String line) throws Exception {
@@ -50,19 +56,30 @@ public class Event extends Task {
         if (desc.trim().isEmpty()) {
             throw new VinException("Description of event not found!! 0.o Please enter a start date after /from");
         }
+
+
         //Case where start date is empty
-        String start = line.substring(line.indexOf(FIRST_SPLITTER) + FIRST_SPLITTER_LENGTH, line.indexOf(SECOND_SPLITTER) - SPACE_LENGTH).trim();
-        if (start.trim().isEmpty()) {
+        String rawStart = line.substring(line.indexOf(FIRST_SPLITTER) + FIRST_SPLITTER_LENGTH, line.indexOf(SECOND_SPLITTER) - SPACE_LENGTH).trim();
+        if (rawStart.trim().isEmpty()) {
             throw new VinException("Start date of event not found!! 0.o Please enter a start date after /from");
         }
+
         //Case where end date is empty
-        String end = line.substring(line.indexOf(SECOND_SPLITTER) + SECOND_SPLITTER_LENGTH).trim();
-        if (end.trim().isEmpty()) {
+        String rawEnd = line.substring(line.indexOf(SECOND_SPLITTER) + SECOND_SPLITTER_LENGTH).trim();
+        if (rawEnd.trim().isEmpty()) {
             throw new VinException("End date of event not found!! 0.o Please enter an end date after /to");
         }
-        //Add event
-        String output = "added: " + desc + "(from: " + start + " to: " + end + ")";
-        UI.printLine(output);
-        return new String[]{desc, start, end};
+        try {
+            //Checks if start and end date follows proper formatting
+            Date start = new Date(rawStart.trim());
+            Date end = new Date(rawEnd.trim());
+            //Add event and store date data as raw data
+            String output = "added: " + desc + "(from: " + start.getDate() + " to: " + end.getDate() + ")";
+            UI.printLine(output);
+            return new String[]{desc, rawStart, rawEnd};
+        }
+        catch (Exception e) {
+            throw new VinException("/from date or /to date does not follow format of DD/MM/YYYY!");
+        }
     }
 }
